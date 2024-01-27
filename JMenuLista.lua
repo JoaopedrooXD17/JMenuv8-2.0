b092a276b3d46a07065e80ceca154a73faa75e43
JoaopedrooXD17 <74885959+JoaopedrooXD17@users.noreply.github.com>
2024-01-26 21:56:06 -0300

    Update JMenuLista.lua

--- JMenuLista.lua
+++ JMenuLista.lua
@@ -162,37 +162,32 @@ local function WVZEMFK_fake_script() -- Jogadoresframe.Main
 	
 end
 coroutine.wrap(WVZEMFK_fake_script)()
+
 local function RAKDQKE_fake_script() -- Jogadoresframe.drag 
 	local script = Instance.new('LocalScript', Jogadoresframe)
 
-	
 	local UIS = game:GetService('UserInputService')
 	local frame = script.Parent
-	local dragToggle = nil
+	local dragToggle = false
 	local dragSpeed = 0.25
 	local dragStart = nil
 	local startPos = nil
-	
+
 	local function updateInput(input)
 		local delta = input.Position - dragStart
 		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
 			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
 		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
 	end
-	
+
 	frame.InputBegan:Connect(function(input)
-		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then 
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then 
 			dragToggle = true
 			dragStart = input.Position
 			startPos = frame.Position
-			input.Changed:Connect(function()
-				if input.UserInputState == Enum.UserInputState.End then
-					dragToggle = false
-				end
-			end)
 		end
 	end)
-	
+
 	UIS.InputChanged:Connect(function(input)
 		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
 			if dragToggle then
@@ -200,6 +195,12 @@ local function RAKDQKE_fake_script() -- Jogadoresframe.drag
 			end
 		end
 	end)
-	
+
+	UIS.InputEnded:Connect(function(input)
+		if input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch then
+			dragToggle = false
+		end
+	end)
+
 end
 coroutine.wrap(RAKDQKE_fake_script)()
